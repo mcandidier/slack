204455545500141c6d0eb88643e70e5fe7f98ff2
mcandidier <mannycandidier@gmail.com>
2017-09-18 16:45:43 +0800

    used nested url routers

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,30 @@
-from rest_framework import routers
 from . import views 
+from rest_framework_nested import routers
+from django.conf.urls import url, include
+
+"""
+API endpoint structure.
+    /companies
+    /companies/{pk}/
+    /companies/{company_pk}/channels
+    /companies/{company_pk}/channels/{pk}/
+    /companies/{company_pk}/channels/{channel_pk}/messages/
+    /companies/{company_pk}/channels/{channel_pk}/members/
+"""
 
 router = routers.DefaultRouter() 
+router.register(r'companies', views.CompanyViewSet, base_name='companies')
 
-router.register(r'channels', views.ChannelViewSet)
-router.register(r'messages', views.MessageViewSet)
-router.register(r'companies', views.CompanyViewSet)
-router.register(r'members', views.CompanyMemberViewSet)
-router.register(r'channel-members', views.ChannelMembersViewSet)
+company_router = routers.NestedSimpleRouter(router, r'companies', lookup='company')
+company_router.register(r'channels', views.ChannelViewSet, base_name='channels')
 
-urlpatterns =  router.urls
+channel_router = routers.NestedSimpleRouter(company_router, r'channels', lookup='channel')
+channel_router.register(r'messages', views.MessageViewSet, base_name='messages')
+channel_router.register(r'members', views.ChannelMembersViewSet, base_name='members')
+
+
+urlpatterns =  [
+    url(r'^', include(router.urls)),
+    url(r'^', include(company_router.urls)),
+    url(r'^', include(channel_router.urls))
+]
--- slack/urls.py
+++ slack/urls.py
@@ -1,18 +1,3 @@
-"""slack URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from app.views import CompanyView
@@ -22,7 +7,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('app.urls', namespace='api')),
     url(r'^config/(?P<company_id>\d+)/$', CompanyView.as_view(), name='config')
 ]
