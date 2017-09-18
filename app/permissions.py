44f721dc418e735ff283226ac855d37ca1ed97bd
mcandidier <mannycandidier@gmail.com>
2017-09-18 16:46:03 +0800

    fixed permission classes

--- app/permissions.py
+++ app/permissions.py
@@ -6,22 +6,17 @@ from django.shortcuts import get_object_or_404
 
 
 class IsCompanyMember(permissions.BasePermission):
-
+    """Check if request/current user is allowed to view the selected company
+    """
     def has_permission(self, request, view):
-        active_company = request.session.get('active_company', None)
-        if active_company is not None:
-            return CompanyMember.objects.filter(member=request.user, company__id=int(active_company))
-        return False
+        return CompanyMember.objects.filter(member=request.user, company=view.selected_company)
 
 
 class IsChannelMember(permissions.BasePermission):
-
+    """Check if request user is a member of selected channel within the compnay
+    """
     def has_permission(self, request, view):
-        name = request.query_params.get('channel', None)
-        if name is None and request.method == 'POST':
-            name = request.data['channel']
-
-        channel = get_object_or_404(Channel, name=name, company__id=int(request.session.get('active_company')))
+        channel = view.selected_channel
         if channel.private:
             channel_members_ids = ChannelMembers.objects.filter(channel=channel).values_list('member__id', flat=True)
             if request.user.id in channel_members_ids:
